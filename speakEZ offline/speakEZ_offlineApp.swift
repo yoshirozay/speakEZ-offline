a4761a61e25017824f03f81524bfefe8da250563
Carson O'Sullivan <66914659+yoshirozay@users.noreply.github.com>
2023-03-01 13:12:51 -0500

    Initial Commit

---
+++ speakEZ offline/speakEZ_offlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  speakEZ_offlineApp.swift
+//  speakEZ offline
+//
+//  Created by Carson O'Sullivan on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct speakEZ_offlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
